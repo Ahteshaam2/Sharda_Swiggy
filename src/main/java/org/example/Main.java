4315a54f6e6df6fd7ae7b8d2a1bb46e2b2c63711
Ahteshaam2 <136442236+Ahteshaam2@users.noreply.github.com>
2023-06-14 13:45:47 +0530

    new

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        System.out.print("Enter your name: ");
+        System.out.print("Enter your names: ");
         String name = reader.readLine();
 
         System.out.print("Enter your age: ");
