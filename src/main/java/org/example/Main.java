b1db2d144363a97a30dd969836e3635cd5b3362e
Ahteshaam2 <136442236+Ahteshaam2@users.noreply.github.com>
2023-06-15 15:09:40 +0530

    new

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,50 +1,46 @@
 package org.example;
 
-import java.io.*;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
-        System.out.println("Welcome to Sharda Swiggy");
-        System.out.println("Today's Special");
-        info("A:/papers/menu.csv");
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Select your Dish:");
-        String dish=sc.nextLine();
-        if (dish.equalsIgnoreCase("Dal Makhni")){
-            System.out.println("Here's the List of Some Famous Restaurants:");
-            info("A:/papers/rest_c.csv");
-        }else if(dish.equalsIgnoreCase("Butter Chicken")){
-            info("A:/papers/rest_d.csv");
+    public static void main(String[] args) {
+        if (!validateCredentials()) {
+            System.out.println("Invalid username or password. Exiting...");
+            return;
         }
-        System.out.println("Select the Restaurant");
-        String res=sc.nextLine();
-        System.out.println("Enter the Payment Mode-");
-        System.out.println("1. Google Pay\n2. Paytm\n3. PayPal");
-        String pay=sc.nextLine();
-        System.out.println("Your Payment is being Processed");
+        else{
 
-        System.out.println("Your Order is being Placed");
+            System.out.println("Welcome to Sharda Swiggy Delivery App Mr. Utkarsh");
 
-        System.out.println("Your order will be ready in 20 minutes");
 
+            // Display menu options
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Available Restaurants Choose from which you wanna enter");
+            Restro.displayMenu("A:\\papers\\Restro.csv");
+            int restro = scanner.nextInt();
+
+
+            System.out.println("following are the available Dishes in your chosen restro");
+            Dish.displayMenu("A:\\papers\\Restro.csv");
+
+
+            scanner.close();
+        }
     }
-    private static void info(String filePath) {
-        String line="";
-        try {
-            BufferedReader reader=new BufferedReader(new FileReader(filePath));
-            while((line =reader.readLine())!= null){
-                String[] row=line.split(",");
-                for (String i: row){
-                    System.out.printf("%-30s",i);
-                }
-                System.out.println();
-            }
-        }
-        catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }catch (IOException e) {
-            throw new RuntimeException(e);
+
+    private static boolean validateCredentials() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your username:");
+        String username = scanner.nextLine();
+
+        System.out.println("Enter your password:");
+        String password = scanner.nextLine();
+
+        // Perform username and password validation
+        if (username.equals("Utkarsh") && password.equals("Tiwari")) {
+            return true;
+        } else {
+            return false;
         }
     }
 }
